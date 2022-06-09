d3cb64c3ab977f325ae4dcdd551e881f71afe005
Kuanysh Yabekov <kuanyshyabekov.dev@gmail.com>
2022-06-09 04:22:50 -0700

    Scanner class test

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        String lesson = scanner.next();
+        System.out.println(lesson);
     }
 }
